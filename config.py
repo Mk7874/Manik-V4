82beb23c0a85fa8eda15abfd792eeac6a2a0e8e4
Mk7874 <boltemk6@gmail.com>
2024-07-16 21:53:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7203810629:AAECSqw858-hQhocGZ6KMuDof2ChK58DB8w")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("ManikXhalder")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("2094826745")
 
 
 
